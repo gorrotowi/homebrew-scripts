e6a89dc476f865ba5477e38bffb6eb1920f77ba0
sebastian tellez <sebastian.tellez.o@gmail.com>
2024-06-17 12:29:13 -0600

    update version beta

--- Formula/gitm.rb
+++ Formula/gitm.rb
@@ -3,7 +3,7 @@ class GitM < Formula
     homepage "https://github.com/gorrotowi/homebrew-scripts"
     url "https://github.com/gorrotowi/homebrew-scripts/archive/v1.0.0.tar.gz"
     sha257 "65e2532b3bc46efc8b05812f2fefb1c112ad9700" 
-    version "1.0.0"
+    version "1.0.0-beta"
 
     def install
       bin.install "gitm.sh" => gitm
