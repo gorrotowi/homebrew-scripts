fb12ea9dd6fd9cb3558be79aabb58750b18cb7f0
sebastian tellez <sebastian.tellez.o@gmail.com>
2024-06-17 12:25:29 -0600

    update sha256

--- Formula/gitm.rb
+++ Formula/gitm.rb
@@ -2,7 +2,7 @@ class GitM < Formula
     desc "Script to configure Git and generate its own GPG key to sign commits"
     homepage "https://github.com/gorrotowi/homebrew-scripts"
     url "https://github.com/gorrotowi/homebrew-scripts/archive/v1.0.0.tar.gz"
-    sha257 "a9c150f28c6772b57263af959ace0ffc5ffcbe31" 
+    sha257 "65e2532b3bc46efc8b05812f2fefb1c112ad9700" 
     version "1.0.0"
 
     def install
